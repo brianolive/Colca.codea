92bdbdd01b5e50fbd99055a03da0b177a255a3c5
Brian Olive <brian.olive@knights.ucf.edu>
2019-02-09 12:27:23 -0500

    Rewrite menu scene to use action method

--- Action.lua
+++ Action.lua
@@ -6,13 +6,19 @@ function action(baseTime)
         local act = t[4]
         local params = t[5] or {}
         
-        params.duration = ((stop - start) + 1) / 60
+        if start ~= nil and stop ~= nil then
+            params.duration = ((stop - start) + 1) / 60
+        end
 
-        if act and time.total == baseTime + start then
+        if act and (time.total == baseTime + start or (time.total > baseTime + start and
+            act:sub(1, 2) == "if")) then
+            
             object[act](params)
         end
         
-        if time.total >= baseTime + start and time.total <= baseTime + stop then
+        if time.total >= baseTime + start and ((stop == nil) or
+            (time.total <= baseTime + stop)) then
+            
             if object["draw"] then
                 object["draw"](params)
             end
--- Menu.lua
+++ Menu.lua
@@ -1,230 +1,354 @@
 function menuScene()
     local scene = {}
+    scene.action = action
     
-    local anim = {
-        colcaFade = 255,
-        colcaStringFade = 255,
-        descendColca = HEIGHT + 200,
-        descendString = HEIGHT + 200,
-        fade = 0,
-        playFade = 0,
-        playColor = color(255, 255, 255, 255),
-        backgroundColor = color(0, 0, 0, 255),
-        leftMountain = 400,
-        rightMountain = -1100,
-        centerMountainFade = 0,
-        finalFadeOut = 0,
-        leftMountainColor = color(68, 121, 59, 255),
-        rightMountainColor = color(58, 91, 53, 255),
-        middleMountainColor = color(255, 255, 255, 0),
-        volcanoRed = color(0, 0, 0, 0)
-    }
+    -- local variables
     local start
+    local stop
+    local action
     local colcaDown = false
     local playStarted
     local playEnded
-    local sceneEnded
-    local exit = false
-    
-    local function fadeOut()
-        tween(1, anim, {finalFadeOut = 255}, tween.easing.linear,
-            function()
-                sceneEnded = time.total
-                scenes.next()
-                exit = true
-            end
-        )
-    end
+
+    -- local functions
     
     function scene.enter()
         return true
     end
     
     function scene.act()
-        background(anim.backgroundColor)
         start = start or time.total
+        action = action or scene.action(start)
 
-        if time.total == start then
-            tween(1, anim.backgroundColor, {r = 0, g = 126, b = 158})
-        end
+        -- start, stop, object (draw), action (tween)
+        action{1, 60, menu, "fadeToColor"}
+        action{61, stop, menu}
+        action{61, 360, menu.colca, "drop"}
+        action{361, stop, menu.colca}
+        action{91, 150, menu.middleMountain, "enter"}
+        action{151, stop, menu.middleMountain}
+        action{91, 150, menu.leftMountain, "enter"}
+        action{151, stop, menu.leftMountain}
+        action{91, 165, menu.rightMountain, "enter"}
+        action{166, stop, menu.rightMountain}
+        action{91, 181, menu.title, "fadeIn"}
+        action{182, stop, menu.title}
+        action{211, 228, menu.playButton, "fadeIn"}
+        action{229, stop, menu.playButton, "ifPressed"}
+        action{229, stop, menu.curtain}
         
-        if playEnded and time.total == playEnded + 60 then
-            pushStyle()
-            
-            fill(212, 212, 212, 255)
-            rect(-5, -5, WIDTH + 10, HEIGHT + 10)
-            
-            popStyle()
-        end
-        
-        if playEnded and time.total == playEnded + 70 then
-            pushStyle()
-            
-            fill(80, 78, 78, 255)
-            rect(-5, -5, WIDTH + 10, HEIGHT + 10)
-            
-            popStyle()
-        end
-        
-        if time.total == start + 60 then
-            tween(5, anim, {descendColca = HEIGHT / 2 - 45, descendString =
-                HEIGHT / 2 - 45}, tween.easing.elasticOut, function() colcaDown =
-                true end)
-        end
-            
-        if time.total == start + 90 then  
-            tween(1, anim, {leftMountain = 0}, tween.easing.bounceOut)
-            tween(1.25, anim, {rightMountain = -600}, tween.easing.bounceOut)
-            tween(1, anim.middleMountainColor, {a = 255}, tween.easing.linear)
-        end
-        
-        if time.total == start + 90 then
-            tween(1.5, anim, {fade = 255}, tween.easing.cubicIn)
-        end
-        
-        if time.total == start + 210 then
-            tween(0.3, anim, {playFade = 255}, tween.easing.cubicIn)
-        end
-        
-        if time.total > start + 60 and not sceneEnded then
-            pushStyle()
-            
-            stroke(245, 218, 0, anim.colcaStringFade)
-            strokeWidth(10)
-            line(WIDTH / 2 - 177, HEIGHT, WIDTH / 2 - 177, anim.descendString + 100)
-            
-            stroke(255, 255, 255, anim.colcaStringFade)
-            strokeWidth(1)
-            line(WIDTH / 2 - 177 - 5, HEIGHT, WIDTH / 2 - 177 - 5, anim.descendString + 100)
-            
-            stroke(245, 218, 0, anim.colcaFade)
-            strokeWidth(20)
-            fill(0, 0, 0, 0)
-            ellipse(WIDTH / 2 - 177, anim.descendColca, 200)
-            
-            stroke(255, 255, 255, anim.colcaFade)
-            strokeWidth(1)
-            fill(0, 0, 0, 0)
-            ellipse(WIDTH / 2 - 177, anim.descendColca, 200)
-            
-            popStyle()
+        --stop = (time.total == start + 500)
+    end
+    
+    function scene.exit()
+        return stop
+    end
+    
+    function scene.touched()
+        -- Do nothing
+    end
+    
+    return scene
+end
 
-            pushStyle()
-            
-            tint(anim.middleMountainColor)
-            sprite("Project:mtn", 600, 100, 1000)
-            
-            popStyle()
-            
-            pushStyle()
-          
-            fill(anim.rightMountainColor)
-            
-            translate(300, 0)
-            rotate(22)
-            rect(0, anim.rightMountain, 1400, 600)
-            rotate(-22)
-            translate(-300, 0)
-            
-            popStyle()
-            
-            pushStyle()
-            
-            fill(anim.leftMountainColor)
-            
-            translate(600, 0)
-            rotate(140)
-            rect(anim.leftMountain, anim.leftMountain, 800, 400)
-            rotate(-140)
-            translate(-600, 0)
-            
-            popStyle()
-        end
-        
-        if time.total > start + 90 and not sceneEnded then
-            tint(255, 255, 255, anim.fade)
-            sprite("Project:c", WIDTH / 2 - 417, HEIGHT / 2 + 120, 175)
-            sprite("Project:l", WIDTH / 2  - 7, HEIGHT / 2 + 230, 11)
-            sprite("Project:c", WIDTH / 2 + 158, HEIGHT / 2 + 120, 175)
-            sprite("Project:a", WIDTH / 2 + 398, HEIGHT / 2 + 120, 245)
-        end
+menu = {
+    backgroundColor = color(0, 0, 0, 255),
+    colca = {
+        dropColca = HEIGHT + 200,
+        dropString = HEIGHT + 200,
+        fade = 255,
+        stringFade = 255,
+        tweens = {}
+    },
+    curtain = {},
+    finalFadeOut = 0,
+    leftMountain = {
+        col = color(68, 121, 59, 255),
+        tweens = {},
+        xy = 400
+    },
+    middleMountain = {
+        col = color(255, 255, 255, 0),
+        tweens = {}
+    },
+    playButton = {
+        alpha = 0,
+        col = color(255, 255, 255),
+        tweens = {}
+    },
+    rightMountain = {
+        col = color(58, 91, 53, 255),
+        tweens = {},
+        y = -1100
+    },
+    title = {
+        col = color(255, 255, 255, 0),
+        tweens = {}
+    },
+    tweens = {},
+    volcanoRed = color(0, 0, 0, 0)
+}
 
-        if time.total > start + 210 and not sceneEnded then
-            pushStyle()
+function menu.fadeToColor(params)
+    menu.tweens.fadeToColor = tween(
+        params.duration,
+        menu.backgroundColor,
+        {r = 0, g = 126, b = 158}
+    )
+end
+
+function menu.draw(params)
+    pushStyle()
+    
+    background(menu.backgroundColor)
+    
+    popStyle()
+end
+
+function menu.colca.drop(params)
+    menu.colca.tweens.drop = tween(
+        params.duration,
+        menu.colca,
+        {dropColca = HEIGHT / 2 - 45, dropString = HEIGHT / 2 - 45},
+        tween.easing.elasticOut,
+        function() colcaDown = true end
+    )
+end
+
+function menu.colca.draw(params)
+    pushStyle()
+    
+    stroke(245, 218, 0, menu.colca.fade)
+    strokeWidth(10)
+    line(WIDTH / 2 - 177, HEIGHT, WIDTH / 2 - 177, menu.colca.dropString + 100)
             
-            textMode(CENTER)
-            fill(anim.playColor.r, anim.playColor.g, anim.playColor.b, anim.playFade)
-            font("Futura-Medium")
-            fontSize(40)
-            text("Play", WIDTH / 2 + 500, HEIGHT / 2 - 300)
+    stroke(255, 255, 255, menu.colca.stringFade)
+    strokeWidth(1)
+    line(WIDTH / 2 - 177 - 5, HEIGHT, WIDTH / 2 - 177 - 5, menu.colca.dropString + 100)
             
-            popStyle()
-        end
+    stroke(245, 218, 0, menu.colca.fade)
+    strokeWidth(20)
+    fill(0, 0, 0, 0)
+    ellipse(WIDTH / 2 - 177, menu.colca.dropColca, 200)
+            
+    stroke(255, 255, 255, menu.colca.fade)
+    strokeWidth(1)
+    fill(0, 0, 0, 0)
+    ellipse(WIDTH / 2 - 177, menu.colca.dropColca, 200)
+    
+    popStyle()
+end
+
+function menu.curtain.draw(params)
+    pushStyle()
         
-        if time.total > start + 240 then
-            if vec2(CurrentTouch.x, CurrentTouch.y):dist(vec2(WIDTH / 2 + 500,
-                HEIGHT / 2 - 300)) < 50 then
+    fill(0, 0, 0, menu.finalFadeOut)
+    rect(-5, -5, WIDTH + 5 , HEIGHT + 5)
+        
+    popStyle()
+end
+
+function menu.leftMountain.enter(params)
+    menu.leftMountain.tweens.enter = tween(
+        params.duration,
+        menu.leftMountain,
+        {xy = 0},
+        tween.easing.bounceOut
+    )
+end
+
+function menu.leftMountain.draw(params)
+    pushStyle()
+    
+    fill(menu.leftMountain.col)
+            
+    translate(600, 0)
+    rotate(140)
+    rect(menu.leftMountain.xy, menu.leftMountain.xy, 800, 400)
+    rotate(-140)
+    translate(-600, 0)
+    
+    popStyle()
+end
+
+function menu.middleMountain.enter(params)
+    menu.middleMountain.tweens.enter = tween(
+        params.duration,
+        menu.middleMountain.col,
+        {a = 255},
+        tween.easing.linear
+    )
+end
+
+function menu.middleMountain.draw()
+    pushStyle()
+            
+    tint(menu.middleMountain.col)
+    sprite("Project:mtn", 600, 100, 1000)
+            
+    popStyle()
+end
+
+function menu.playButton.fadeIn(params)
+    menu.playButton.tweens.fadeIn = tween(
+        params.duration,
+        menu.playButton,
+        {alpha = 255},
+        tween.easing.cubicIn
+    )
+end
+
+function menu.playButton.ifPressed(params)
+    if vec2(CurrentTouch.x, CurrentTouch.y):dist(vec2(WIDTH / 2 + 500,
+        HEIGHT / 2 - 300)) < 50 then
+        
+        if playStarted == nil then
+            tween(
+                0.1,
+                menu.playButton.col,
+                {r = 245, g = 218, b = 0}
+            )
+            playStarted = true
+        end
                 
-                if playStarted == nil then
-                    tween(0.1, anim.playColor, {r = 245, g = 218, b = 0})
-                    playStarted = true
-                end
-                
-                if CurrentTouch.state == ENDED and colcaDown == true and
-                    playEnded == nil then
+        if CurrentTouch.state == ENDED and colcaDown == true and
+            playEnded == nil then
                     
-                    --tween(0.1, anim, {colcaStringFade = 0}, tween.easing.linear)
-                    anim.colcaStringFade = 0  
-                    tween(1.0, anim, {
-                        descendColca = -200,
-                        fade = 0,
-                        colcaFade = 0,
-                        playFade = 0
-                    }, tween.easing.linear)
-                    tween(1.75, anim.backgroundColor, {r = 10, g = 0, b = 0},
-                        tween.easing.linear)
-                    tween(2.5, anim.leftMountainColor, {r = 50, g = 50, b = 50},
-                        tween.easing.linear)
-                    tween(2.5, anim.rightMountainColor, {r = 30, g = 30, b = 30},
-                        tween.easing.linear)
-                    tween(2.5, anim.middleMountainColor, {r = 25, g = 15, b = 15},
-                        tween.easing.linear)
-                    playEnded = time.total
-                end
-            end
+            menu.colca.stringFade = 0  
+            tween(
+                1.0,
+                menu.colca,
+                {dropColca = -200, fade = 0},
+                tween.easing.linear
+            )
+            tween(
+                1.0,
+                menu.title.col,
+                {a = 0},
+                tween.easing.linear
+            )
+            tween(
+                1.0,
+                menu.playButton,
+                {alpha = 0},
+                tween.easing.linear
+            )
+            tween(
+                1.75,
+                menu.backgroundColor,
+                {r = 10, g = 0, b = 0},
+                tween.easing.linear
+            )
+            tween(
+                2.5,
+                menu.leftMountain.col,
+                {r = 50, g = 50, b = 50},
+                tween.easing.linear
+            )
+            tween(
+                2.5,
+                menu.rightMountain.col,
+                {r = 30, g = 30, b = 30},
+                tween.easing.linear
+            )
+            tween(
+                2.5,
+                menu.middleMountain.col,
+                {r = 25, g = 15, b = 15},
+                tween.easing.linear
+            )
+            
+            playEnded = time.total
         end
-        
-        if playEnded and time.total == playEnded + 30 then
-            tween(4, anim.volcanoRed, {r = 124, g = 31, b = 33, a = 50},
-                tween.easing.linear, fadeOut)
+    end
+    
+    if playEnded and time.total == playEnded + 30 then
+            tween(
+                4,
+                menu.volcanoRed,
+                {r = 124, g = 31, b = 33, a = 50},
+                tween.easing.linear,
+                function()
+                    tween(
+                        1,
+                        menu,
+                        {finalFadeOut = 255},
+                        tween.easing.linear,
+                        function()
+                            sceneEnded = time.total
+                            scenes.next()
+                            exit = true
+                        end
+                    )
+                end
+            )
         end
         
         if playEnded and time.total > playEnded + 30 then
             pushStyle()
             
-            fill(anim.volcanoRed)
+            fill(menu.volcanoRed)
             ellipse(WIDTH / 2 - 47, 256, 20, 10)
-            fill(25, 18, 18, anim.volcanoRed.a)
+            fill(25, 18, 18, menu.volcanoRed.a)
             ellipse(WIDTH / 2 - 47, 256, 10, 5)
             
             popStyle()
         end
-        
-        pushStyle()
-        
-        fill(0, 0, 0, anim.finalFadeOut)
-        rect(-5, -5, WIDTH + 5 , HEIGHT + 5)
-        
-        popStyle()
-    end
+end
+
+function menu.playButton.draw()
+    pushStyle()
     
-    function scene.exit()      
-        return exit
-    end
+    textMode(CENTER)
+    fill(menu.playButton.col.r, menu.playButton.col.g, menu.playButton.col.b,
+        menu.playButton.alpha)
+    font("Futura-Medium")
+    fontSize(40)
+    text("Play", WIDTH / 2 + 500, HEIGHT / 2 - 300)    
     
-    function scene.touched(t)
-        
-    end
+    popStyle()
+end
+
+function menu.rightMountain.enter(params)
+    menu.rightMountain.tweens.enter = tween(
+        params.duration,
+        menu.rightMountain,
+        {y = -600},
+        tween.easing.bounceOut
+    )
+end
+
+function menu.rightMountain.draw(params)
+    pushStyle()
     
-    return scene
+    fill(menu.rightMountain.col)
+            
+    translate(300, 0)
+    rotate(22)
+    rect(0, menu.rightMountain.y, 1400, 600)
+    rotate(-22)
+    translate(-300, 0)
+    
+    popStyle()
+end
+
+function menu.title.fadeIn(params)
+    menu.title.tweens.fadeIn = tween(
+        params.duration,
+        menu.title.col,
+        {a = 255},
+        tween.easing.cubicIn
+    )
+end
+
+function menu.title.draw()
+    pushStyle()
+    
+    tint(menu.title.col)
+    sprite("Project:c", WIDTH / 2 - 417, HEIGHT / 2 + 120, 175)
+    sprite("Project:l", WIDTH / 2  - 7, HEIGHT / 2 + 230, 11)
+    sprite("Project:c", WIDTH / 2 + 158, HEIGHT / 2 + 120, 175)
+    sprite("Project:a", WIDTH / 2 + 398, HEIGHT / 2 + 120, 245)
+    
+    popStyle()
 end
